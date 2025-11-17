b0ff0db0939422f05a59f9bca20659fc1ac4fc8a
Giassinne <yassinekaddouri2711@gmail.com>
2025-11-17 10:09:54 +0100

    Initial commit

---
+++ src/main/java/com/example/TickNet/TickNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.TickNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickNetApplication.class, args);
+	}
+
+}
